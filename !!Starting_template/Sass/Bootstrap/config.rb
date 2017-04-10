045ed8342d1eb1f6b0fb876f9b286a083d75c04d
k241285@mail.ru <aleksandr.chasovnikov@gmail.com>
2017-04-10 22:12:57 +0300

    super refactoring

--- !!Starting_template/Sass/Bootstrap/config.rb
+++ !!Starting_template/Sass/Bootstrap/config.rb
@@ -1,4 +1,4 @@
-compasscrequire 'compass/import-once/activate'
+require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
